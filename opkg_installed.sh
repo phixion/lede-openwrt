894fbd6cf2e1b2b4f99d3c83aa1e15450e84d6c4
Phixion <ms@phixion.org>
2017-10-08 07:57:20 +0200

    much more cleaner

--- opkg_installed.sh
+++ opkg_installed.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
-#collect installed openwrt packages
-#reinstall after update with opkg update && opkg install `cat /etc/config/packlist.txt` && reboot
+# collect installed openwrt packages
+# reinstall after update with opkg update && opkg install `cat /etc/config/packlist.txt` && reboot
 
-FLASH_TIME=$(opkg info busybox | grep '^Installed-Time: ')
-
-for i in $(opkg list-installed | cut -d' ' -f1)
-do
-    if [ "$(opkg info $i | grep '^Installed-Time: ')" != "$FLASH_TIME" ]
-    then
-        echo $i
-    fi
-done
+opkg list-installed | awk '{print $1}' | sed ':M;N;$!bM;s#\n# #g' > /etc/config/packlist.txt
